82da8398f2742dbea9afe97cf16c671ea8205f9e
brian <brian@brian-desktop>
2014-08-30 11:04:48 -0500

    put my name instead of octocat's.

---
+++ main.cpp
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.cpp
+ * Author: brian
+ *
+ * Created on August 30, 2014, 10:55 AM
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    std::cout << "hello world!" << std::endl;
+    return 0;
+}
+
